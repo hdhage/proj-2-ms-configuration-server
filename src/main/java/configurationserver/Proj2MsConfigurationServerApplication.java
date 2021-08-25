ce8b0fc6bab11a889accf717f8bb031d79791376
Hemant D <hemantdhage20@gmail.com>
2021-08-25 20:10:11 +0530

    Initial commit

---
+++ src/main/java/configurationserver/Proj2MsConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj2MsConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj2MsConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/configurationserver/Proj2MsConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj2MsConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
